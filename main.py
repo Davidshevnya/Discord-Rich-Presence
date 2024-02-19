4925e0de76415c5b1b0426eb17dc9a40a56568c0
David <dawidshevnya@gmail.com>
2024-02-19 11:33:48 +0200

    Refactoring code

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 from pypresence import Presence
-import json, time
+import json
 
 
 def updateRPC():
-    with open("config.json") as config_file:
-        config = config_file.read()
-        config = json.loads(config)
-    RPC = Presence(config.get("ClientID"))
-    RPC.connect()
+    try:
+        with open("config.json") as config_file:
+            config = config_file.read()
+            config = json.loads(config)
+        RPC = Presence(config.get("ClientID"))
+        # config.pop("ClientID")
+        RPC.connect()
+        
+        RPC.update(**config)
+    except Exception as _ex:
+        return print(f"[ERROR] {_ex}")
+    
     print("[INFO] Discord Rich Presence активировано")
     print("[WARNING] Не закрывайте эту консоль, так как она завершит работу RPC.")
-    RPC.update(state=config.get("State"), details=config.get("Details"),
-               start=time.time(), large_image=config.get("LargeImage"),
-                large_text=config.get("LargeImageText"), small_image=config.get("SmallImage"),
-                small_text=config.get("SmallImageText"), buttons = [
-                    {"label": config.get("Button1"), "url": config.get("Url1")},
-                    {"label": config.get("Button2"), "url": config.get("Url2")}
-                ]
-    )
-
 
 if __name__ == "__main__":
     updateRPC()
